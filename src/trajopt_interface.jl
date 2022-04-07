fc3b8f7472666738d34b2290a2c930cb3ddc5d8d
Brian Jackson <bjack205@gmail.com>
2022-04-07 13:48:15 -0400

    dubins w/ control constraints

--- src/trajopt_interface.jl
+++ src/trajopt_interface.jl
@@ -7,8 +7,19 @@ function BilinearADMM(prob::TO.Problem)
     
     # Extract quadratic cost
     Q,q,R,r,c = BilinearControl.buildcostmatrices(prob)
+    
+    # Extract control bounds
+    ucon = filter(x->x isa TO.BoundConstraint, TO.get_constraints(prob).constraints)
+    if !isempty(ucon)
+        n = RD.state_dim(prob, 1)
+        ulo = TO.lower_bound(ucon[1])[n+1:end]
+        uhi = TO.upper_bound(ucon[1])[n+1:end]
+    else
+        ulo = -Inf
+        uhi = Inf
+    end
 
-    admm = BilinearADMM(A,B,C,D, Q,q,R,r,c)
+    admm = BilinearADMM(A,B,C,D, Q,q,R,r,c, umin=ulo, umax=uhi)
     admm.opts.penalty_threshold = 1e4
     BilinearControl.setpenalty!(admm, 1e3)
     admm
--- test/dubins_test.jl
+++ test/dubins_test.jl
@@ -11,7 +11,7 @@ function testdynamics()
     @test RD.dynamics(model, y, u) ≈ A*y + B*u + sum(u[i]*C[i]*y for i = 1:nu) + D
 end
 
-function builddubinsproblem()
+function builddubinsproblem(ubnd=Inf)
     # Model
     model = BilinearDubins()
     dmodel = RD.DiscretizedDynamics{RD.ImplicitMidpoint}(model)
@@ -38,17 +38,22 @@ function builddubinsproblem()
     cons = ConstraintList(nx, nu, N)
     goalcon = GoalConstraint(xf)  # only constraint the original states
     add_constraint!(cons, goalcon, N)
+    
+    # Control bound constraints
+    add_constraint!(cons, BoundConstraint(nx, nu, u_min=-ubnd, u_max=ubnd), 1:N-1)
 
     # Initial Guess
     U0 = [[0.1,0] for k = 1:N-1] 
 
     # Build the problem
-    Problem(dmodel, obj, x0, tf, xf=xf, constraints=cons, U0=U0)
+    prob = Problem(dmodel, obj, x0, tf, xf=xf, constraints=cons, U0=U0)
+    rollout!(prob)
+    prob
 end
 
+@testset "Dubins Dynamics" begin
 testdynamics()
 prob = builddubinsproblem()
-rollout!(prob)
 A,B,C,D = BilinearControl.buildbilinearconstraintmatrices(
     prob.model[1].continuous_dynamics, prob.x0, prob.xf, prob.Z[1].dt, prob.N
 )
@@ -57,11 +62,16 @@ U = vcat(Vector.(controls(prob))...)
 c1 = A*X + B*U + sum(U[i] * C[i] * X for i = 1:length(U)) + D
 c2 = BilinearControl.evaluatebilinearconstraint(prob)
 @test c1 ≈ c2
+end
 
-Q,q,R,r,c = BilinearControl.buildcostmatrices(prob)
-admm = BilinearADMM(A,B,C,D, Q,q,R,r,c)
-admm.opts.penalty_threshold = 1e4
-BilinearControl.setpenalty!(admm, 1e3)
+## Run ADMM
+@testset "Dubins (unconstrained)" begin
+prob = builddubinsproblem()
+admm = BilinearADMM(prob)
+admm.opts.penalty_threshold = 1e2
+BilinearControl.setpenalty!(admm, 1e4)
+X = extractstatevec(prob)
+U = extractcontrolvec(prob)
 Xsol, Usol = BilinearControl.solve(admm, X, U, max_iters=300)
 n,m = RD.dims(prob.model[1])
 
@@ -76,11 +86,51 @@ zterm = Xsol[end-1:end]
 @test abs(zterm'*[0,1] - 1.0) < 1e-4
 
 # Check that the norm is preserved
-@test all(x->abs(x-1.0) < 1e-4, [norm(x[3:4]) for x in eachcol(reshape(Xsol,n,:))])
+normerr = maximum([norm(x[3:4]) - 1 for x in eachcol(reshape(Xsol,n,:))])
+@test normerr < 1e-2
 
 # Check that the control signals are smooth 
 Us = reshape(Usol, m, :)
 @test all(x->x< 1e-2, mean(diff(Us, dims=2), dims=2))
 
+global umax0 = norm(Us,Inf)
+end
 
+## Check with constraints
+@testset "Dubins w/ Control Constraints" begin
+ubnd = 1.0
+prob = builddubinsproblem(ubnd)
+admm = BilinearADMM(prob)
+@test admm.ulo == fill(-ubnd, length(admm.z))
+@test admm.uhi == fill(+ubnd, length(admm.z))
+admm.opts.penalty_threshold = 1e4
+BilinearControl.setpenalty!(admm, 1e3)
+X = extractstatevec(prob)
+U = extractcontrolvec(prob)
+Xsol, Usol = BilinearControl.solve(admm,X,U, max_iters=100)
 
+# Make sure it made it to the goal
+n,m = RD.dims(prob.model[1])
+xtraj = reshape(Xsol,n,:)[1,:]
+ytraj = reshape(Xsol,n,:)[2,:]
+@test abs(xtraj[end] - prob.xf[1]) < 1e-4
+@test abs(ytraj[end] - prob.xf[1]) < 1e-4
+
+# Check the terminal heading
+zterm = Xsol[end-1:end]
+@test abs(zterm'*[0,1] - 1.0) < 1e-4
+
+# Check that the norm is preserved
+normerr = maximum([norm(x[3:4]) - 1 for x in eachcol(reshape(Xsol,n,:))])
+@test normerr < 1e-2
+
+# Check that the control signals are smooth 
+Us = reshape(Usol, m, :)
+@test all(x->x< 1e-2, mean(diff(Us, dims=2), dims=2))
+Us = reshape(Usol, m, :)
+
+# Check maximum control
+umax = norm(Us,Inf)
+@test umax < umax0*0.9
+@test abs(umax - ubnd) < 1e-3
+end
