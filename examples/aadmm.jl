c3f13186b3c1187cdd617439f9800ce0b44525e5
Brian Jackson <bjack205@gmail.com>
2022-04-08 15:25:08 -0400

    AADMM working on toy example

--- examples/aadmm.jl
+++ examples/aadmm.jl
@@ -4,34 +4,27 @@ using ForwardDiff
 using Printf
 using Random
 
-function newton(f, x0; tol=1e-12, max_iters=20)
-    H(x) = ForwardDiff.hessian(f, x) 
-    g(x) = ForwardDiff.gradient(f, x) 
-    x = copy(x0)
-    for i = 1:max_iters
-        b = g(x)
-        if norm(b) < tol
-            return x
-        end
-        A = H(x)
-        dx = -(A \ b)
-        x .+= dx
-    end
-    error("newton failed to converge")
-end
-
 struct ADMMProblem{T}
     Q::Diagonal{T,Vector{T}}
     R::Diagonal{T,Vector{T}}
     q::Vector{T}
     r::Vector{T}
+    A0::Matrix{T}
+    B0::Matrix{T}
+    C::Vector{Matrix{T}}
+    b::Vector{T}
     A::Matrix{T}
     B::Matrix{T}
-    b::Vector{T}
+end
+function ADMMProblem(Q,R,q,r, A,B,C,b)
+    Ahat = zero(A)
+    Bhat = zero(B)
+    ADMMProblem(Q,R,q,r,A,B,C,b,Ahat,Bhat)
 end
 
 eval_f(prob::ADMMProblem, x) = 0.5 * dot(x, prob.Q, x) + dot(prob.q, x)
 eval_g(prob::ADMMProblem, z) = 0.5 * dot(z, prob.R, z) + dot(prob.r, z)
+eval_c(prob::ADMMProblem, x, z) = prob.A0*x + prob.B0*z + sum(z[i]*prob.C[i]*x for i in eachindex(z)) - prob.b
 
 function auglag(prob::ADMMProblem, x, z, λ, ρ)
     r = prob.b - prob.A*x - prob.B*z + λ / ρ
@@ -40,18 +33,29 @@ end
 
 function solvex(prob::ADMMProblem, x, z, λ, ρ)
     H = prob.Q + ρ * prob.A'prob.A
-    b = prob.q + ρ * prob.A'*(prob.B*z - prob.b + λ/ρ)
+    b = prob.q + ρ * prob.A'*(prob.B0*z - prob.b + λ/ρ)
     return -(H\b)
 end
 
 function solvez(prob::ADMMProblem, x, z, λ, ρ)
     H = prob.R + ρ * prob.B'prob.B
-    b = prob.r + ρ * prob.B'*(prob.A*x - prob.b + λ/ρ)
+    b = prob.r + ρ * prob.B'*(prob.A0*x - prob.b + λ/ρ)
     return -(H\b)
 end
 
 function dualupdate(prob::ADMMProblem, x, z, λ, ρ)
-    λ + ρ * (prob.A*x + prob.B*z - prob.b) 
+    λ + ρ * eval_c(prob, x, z) 
+end
+
+function updateA!(prob::ADMMProblem, z)
+    prob.A .= prob.A0 .+ sum(prob.C[i] * z[i] for i in eachindex(z))
+end
+
+function updateB!(prob::ADMMProblem, x)
+    prob.B .= prob.B0
+    for i = 1:size(prob.B, 2)
+        prob.B[:,i] .+= prob.C[i] * x
+    end
 end
 
 function calcstep(H, y)
@@ -70,7 +74,8 @@ function solve(prob, x0, z0, λ0, ρ;
         max_iters=100, 
         eps_primal=1e-3, 
         eps_dual=1e-2,
-        Tf = 2
+        Tf = 2,
+        ϵ_cor = 0.2 
     )
     x = copy(x0)
     z = copy(z0)
@@ -83,17 +88,19 @@ function solve(prob, x0, z0, λ0, ρ;
     z0 = copy(z)
     λ0 = copy(λ)
     λhat0 = copy(λ)
-    ϵ_cor = 0.2 
 
     @printf("%8s %10s %10s %10s, %10s\n", "iter", "cost", "||r||", "||s||", "ρ")
 
     A,B,b = prob.A, prob.B, prob.b
     for iter = 1:max_iters
+        updateA!(prob, z)
         xn .= solvex(prob, x, z, λ, ρ)
+        updateB!(prob, xn)
         zn .= solvez(prob, xn, z, λ, ρ)
+        updateA!(prob, zn)
         λn .= dualupdate(prob, xn, zn, λ, ρ) 
 
-        r = A*xn + B*zn - b  
+        r = eval_c(prob, xn, zn) 
         s = ρ * A'B*(zn - z)
         J = eval_f(prob, x) + eval_g(prob, z)
         @printf(
@@ -106,7 +113,7 @@ function solve(prob, x0, z0, λ0, ρ;
 
         # Penalty update
         if iter % Tf == 0
-            λhat = λ + ρ * (A*xn + B*z - b)
+            λhat = λ + ρ * eval_c(prob, xn, z) 
             Δλhat = λhat - λhat0
             ΔH = A*(xn - x0)
             α, α_cor = calcstep(ΔH, Δλhat)
@@ -137,7 +144,8 @@ end
 
 
 ##
-Random.seed!(1)
+Random.seed!(2)
+n,m,p = 10,5,5
 prob = ADMMProblem(
     Diagonal(fill(1.0, n)), 
     Diagonal(fill(0.1, m)), 
@@ -145,11 +153,12 @@ prob = ADMMProblem(
     randn(m), 
     randn(p,n),
     randn(p,m),
+    [randn(p,n)*1 for i = 1:m],
     randn(p)
 )
 x = randn(n)
 z = randn(m)
 λ = zeros(p)
 ρ = 1.0
-solve(prob, x, z, λ, ρ, max_iters=200)
-solve(prob, x, z, λ, ρ, max_iters=200, Tf=typemax(Int))
+# solve(prob, x, z, λ, ρ, max_iters=4000, Tf=typemax(Int))
+solve(prob, x, z, λ, ρ, max_iters=4000, ϵ_cor=0.2, Tf=5)
