8f3ce8b34fce60711f95573d3ee34641b92b2c6f
Brian Jackson <bjack205@gmail.com>
2022-04-21 14:01:09 -0400

    swarm almost working

--- dev/swarm/swarm_dynamics_test.jl
+++ dev/swarm/swarm_dynamics_test.jl
@@ -10,6 +10,7 @@ using BilinearControl: getA, getB, getC, getD
 using Test
 
 include("swarm_model.jl")
+include(joinpath(@__DIR__, "..", "..", "examples", "visualization", "visualization.jl"))
 
 P = 4
 model0 = BilinearDubins() 
@@ -47,26 +48,88 @@ A,B,C,D = getA(model), getB(model), getC(model), getD(model)
 @test xdot ≈ A*x + B*u + sum(u[i]*C[i] * x for i = 1:length(u)) + D
 
 # Formation constraints
-x1 = vec(Float64[
+x0 = vec(Float64[
     0 0 1 1
     1 0 1 0
     0 0 0 0
     1 1 1 1
 ])
-x2 = vec(Float64[
+x1 = vec(Float64[
     1 0 1 0
     0 0 -1 -1 
     1 1 1 1
     0 0 0 0
 ])
-visualize!(vis, model, x1)
+xf = vec(Float64[
+    0 0 1 1
+    2 1 2 1
+    0 0 0 0
+    1 1 1 1
+])
+visualize!(vis, model, xf)
 
 relcons = [
-    (i=1,j=2,x=-1,y=0),
-    (i=1,j=3,x=0,y=-1),
-    (i=2,j=4,x=0,y=-1),
+    (i=1,j=2,x=-1,y=0, α=1, β=0),
+    (i=1,j=3,x=0,y=-1, α=1, β=0),
+    (i=2,j=4,x=0,y=-1, α=1, β=0),
+    # (i=3,j=4,x=-1,y=0, α=1, β=0),
 ]
 F = buildformationconstraint(model, relcons)
 norm(F*x) > 1e-3
-norm(F*x1) < 1e-8
+norm(F*x0) < 1e-8
 norm(F*x2) < 1e-8
+
+# Bilinear Constraint (includes formation constraints)
+Af = [
+    Matrix(I,4, 4P);
+    F
+]
+bf = [-xf[1:4]; zeros(size(F,1))]
+
+N = 51
+tf = 3.0
+h = tf / (N-1)
+Abar,Bbar,Cbar,Dbar = BilinearControl.buildbilinearconstraintmatrices(model, x0, Af, bf, h, N)
+
+Xs = [rand(model)[1] for k = 1:N]
+Us = [rand(model)[2] for k = 1:N-1]
+X = vcat(Xs...)
+U = vcat(Us...)
+
+p = size(F,1)
+c = Abar*X + Bbar*U + sum(U[i] * Cbar[i] * X for i = 1:length(U)) + Dbar
+@test c[1:4P] ≈ x0 - Xs[1]
+@test c[4P+1:8P] ≈ h*(A*(Xs[1] + Xs[2])/2 + B*Us[1] + sum(Us[1][i] * C[i] * (Xs[1]+Xs[2])/2 for i = 1:2P) + D) + Xs[1] - Xs[2]
+p = 0
+@test all(1:N-2) do k
+    c[8P + (k-1)*(4P + p) .+ (1:4P)] ≈ h*(A*(Xs[k+1] + Xs[k+2])/2 + B*Us[k+1] + sum(Us[k+1][i] * C[i] * (Xs[k+1]+Xs[k+2])/2 for i = 1:2P) + D) + Xs[k+1] - Xs[k+2]
+end
+# @test all(1:N-2) do k
+#     c[12P + (k-1)*(4P + p) .+ (1:p)] ≈ F*Xs[k+1]
+# end
+@test c[end-size(Af,1)+1:end] ≈ Af*Xs[end] + bf
+
+# Objective 
+Q = Diagonal(repeat([1,1,0.1,0.1], P))
+R = Diagonal(repeat([0.1,0.1], P))
+Qf = Q * (N-1)
+Qbar,Rbar,qbar,rbar,cbar = BilinearControl.buildcostmatrices(Q,R,Qf,xf,N)
+
+Qbar = blockdiag([sparse(F'F) for k = 1:N]...)
+qbar = zeros(4P*N)
+
+# Initial trajectory
+Xs = map(range(0,1,length=N)) do θ 
+    x0 .+ (xf .- x0) .* θ
+end
+X = vcat(Xs...)
+U = zeros((N-1)*2P)
+
+# ADMM Solver
+solver = BilinearADMM(Abar, Bbar, Cbar, Dbar, Qbar, qbar, Rbar, rbar, cbar)
+BilinearControl.setpenalty!(solver, 1e4)
+solver.opts.penalty_threshold = 1e2
+Xsol, Usol = BilinearControl.solve(solver, X, U, verbose=true, max_iters=500)
+
+Xs = collect(eachcol(reshape(Xsol, 4P, :)))
+visualize!(vis, model, tf, Xs)
--- dev/swarm/swarm_model.jl
+++ dev/swarm/swarm_model.jl
@@ -76,11 +76,12 @@ BilinearControl.getD(::SwarmSE2{P}) where P = zeros(4P)
 
 function buildformationconstraint(::SwarmSE2{P}, cons)
     ncons = length(cons)
-    F = zeros(2ncons, 4P)
+    F = zeros(4ncons, 4P)
     for k = 1:ncons
         i,j = cons[k].i, cons[k].j
         x,y = cons[k].x, cons[k].y
-        Fi = view(F, (k-1)*2 .+ (1:2), (i-1)*4 .+ (1:4))
+        α,β = cons[k].α, cons[k].β
+        Fi = view(F, (k-1)*4 .+ (1:4), (i-1)*4 .+ (1:4))
         Fi[1,1] = 1
         Fi[2,2] = 1
         Fi[1,3] = x
@@ -88,10 +89,73 @@ function buildformationconstraint(::SwarmSE2{P}, cons)
         Fi[1,4] = -y
         Fi[2,3] = y
 
-        Gi = view(F, (k-1)*2 .+ (1:2), (j-1)*4 .+ (1:4))
+        Gi = view(F, (k-1)*4 .+ (1:4), (j-1)*4 .+ (1:4))
         Gi[1,1] = -1
         Gi[2,2] = -1
         C[1 + (i-1)*2]
     end
     F
+end
+
+function BilinearControl.buildbilinearconstraintmatrices(model::SwarmSE2{P}, x0, Af, bf, h, N; 
+        relcons=[]
+    )
+    n,m = RD.dims(model)
+    ncons = length(relcons)
+    Nx = N*n
+    Nu = (N-1)*m
+    Nc = length(x0) + size(Af,1) + (N-1) * n + (N-2)*2*ncons
+
+    ic = 1:n
+    ix1 = 1:n
+    ix2 = ix1 .+ n
+    iu1 = 1:m
+
+    # Build matrices
+    Abar = spzeros(Nc,Nx)
+    Bbar = spzeros(Nc,Nu)
+    Cbar = [spzeros(Nc,Nx) for i = 1:Nu]
+    Dbar = spzeros(Nc)
+
+    # Initial condition
+    Abar[ic, ix1] .= -I(n)
+    Dbar[ic] .= x0
+    ic = ic[end] .+ (1:n)
+
+    # Dynamics and Formation
+    A,B,C,D = getA(model), getB(model), getC(model), getD(model)
+    F = buildformationconstraint(model, relcons)
+    for k = 1:N-1
+        # Dynamics
+        Abar[ic, ix1] .= h/2 * A + I
+        Abar[ic, ix2] .= h/2 * A - I
+        Bbar[ic, iu1] .= h*B
+        for (i,j) in enumerate(iu1)
+            Cbar[j][ic,ix1] .= h/2 * C[i]
+            Cbar[j][ic,ix2] .= h/2 * C[i]
+        end
+        Dbar[ic] .= h*D
+
+        if k > 1
+            # Formation constraint
+            ic2 = ic[end] .+ (1:2ncons)
+            Abar[ic2, ix1] .= F
+        else
+            ic2 = ic[end]+1:ic[end]
+        end
+
+        @show ic
+        ic = ic2.stop .+ (1:n)
+        ix1 = ix1 .+ n
+        ix2 = ix2 .+ n
+        iu1 = iu1 .+ m 
+    end
+
+    # Terminal constraint
+    @show ic
+    ic = ic[1] - 1 .+ (1:size(Af,1))
+    Abar[ic, ix1] .= Af
+    Dbar[ic] .= bf
+
+    Abar, Bbar, Cbar, Dbar
 end
