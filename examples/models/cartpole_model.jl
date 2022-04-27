f5832365e371c771e90806416c75ae333e1b7851
Brian Jackson <bjack205@gmail.com>
2022-04-27 12:38:35 -0400

    Test bilinear cartpole model

--- examples/models/cartpole_model.jl
+++ examples/models/cartpole_model.jl
@@ -3,13 +3,19 @@ struct BilinearCartpole <: RD.DiscreteDynamics
     C::Matrix{Float64}
     g::Matrix{Float64}  # mapping from extended to original states
     dt::Float64
+    kf::Function
     function BilinearCartpole()
-        datadir = joinpiath(@__DIR__, "..", "..", "data")
+        datadir = joinpath(@__DIR__, "..", "..", "data")
         data = load(joinpath(datadir, "cartpole_eDMD_data.jld2"))
         A = Matrix(data["F"])
         C = Matrix(data["C"])
         g = Matrix(data["g"])
-        new(A, C, g)
+        T_ref = data["T_ref"]
+        dt = T_ref[2] - T_ref[1]
+        eigfuns = data["eigfuns"]
+        eigorders = data["eigorders"]
+        kf(x) = BilinearControl.EDMD.koopman_transform(x, eigfuns, eigorders)
+        new(A, C, g, dt, kf)
     end
 end
 
@@ -19,12 +25,12 @@ RD.control_dim(model::BilinearCartpole) = 1
 RD.default_diffmethod(::BilinearCartpole) = RD.UserDefined()
 RD.default_signature(::BilinearCartpole) = RD.InPlace()
 
-function RD.dynamics(model::BilinearCartpole, x, u, t, h)
+function RD.discrete_dynamics(model::BilinearCartpole, x, u, t, h)
     @assert h ≈ model.dt "Timestep must be $(model.dt)."
     return model.A*x + model.C*x * u[1]
 end
 
-function RD.dynamics!(model::BilinearCartpole, xn, x, u, t, h)
+function RD.discrete_dynamics!(model::BilinearCartpole, xn, x, u, t, h)
     @assert h ≈ model.dt "Timestep must be $(model.dt)."
     mul!(xn, model.A, x)
     mul!(xn, model.C, x, u[1], true)
@@ -39,3 +45,5 @@ function RD.jacobian!(model::BilinearCartpole, J, xn, x, u, t, h)
     mul!(Ju, model.C, x)
     nothing
 end
+
+expandstate(model::BilinearCartpole, x) = model.kf(x)
--- test/edmd_test.jl
+++ test/edmd_test.jl
@@ -51,11 +51,14 @@ dmodel = RD.DiscretizedDynamics{RD.RK4}(model)
 n,m = RD.dims(model)
 
 ## Learn the bilinear dynamics
-Z_sim, Zu_sim, z0 = BilinearControl.EDMD.build_eigenfunctions(X_ref, U_ref, ["state", "sine", "cosine"], [0,0,0])
+Z_sim, Zu_sim, z0, kf = BilinearControl.EDMD.build_eigenfunctions(X_ref, U_ref, ["state", "sine", "cosine"], [0,0,0])
 F, C, g = BilinearControl.EDMD.learn_bilinear_model(
     X_ref, Z_sim, Zu_sim, ["lasso", "lasso"]; 
     edmd_weights=[0.0], mapping_weights=[0.0]
 )
+
+# Check the koopman transform function
+@test all(k->Z_sim[k] ≈ kf(X_ref[k]), eachindex(Z_sim))
 
 ## Compare solutions
 bi_X, bi_Z = simulate_bilinear(F, C, g, x0, z0, U_ref)
@@ -74,3 +77,23 @@ end
 
 # Test that the trajectories are similar
 @test norm(bi_X - X_ref, Inf) < 0.2
+
+## Load Bilinear Cartpole Model
+model_bilinear = Problems.BilinearCartpole()
+@test RD.discrete_dynamics(model_bilinear, bi_Z[1], U_ref[1], 0.0, dt) ≈ bi_Z[2]
+
+n2,m2 = RD.dims(model_bilinear)
+J = zeros(n2, n2+m2)
+y = zeros(n2)
+z2 = KnotPoint(n2,m2,[bi_Z[1]; U_ref[1]], 0.0, dt)
+RD.jacobian!(RD.InPlace(), RD.UserDefined(), model_bilinear, J, y, z2)
+@test J ≈ [F+C*U_ref[1][1] C*bi_Z[1]]
+
+Jfd = zero(J)
+FiniteDiff.finite_difference_jacobian!(Jfd, 
+    (y,x)->RD.discrete_dynamics!(model_bilinear, y, x[1:n2], x[n2+1:end], 0.0, dt),
+    z2.z
+)
+@test Jfd ≈ J
+
+@test BilinearControl.Problems.expandstate(model_bilinear, X_ref[1]) ≈ Z_sim[1]
