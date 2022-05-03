0478a8c1f20b58ae42f6222808132b030fc6ce35
Brian Jackson <bjack205@gmail.com>
2022-05-03 18:22:33 -0400

    Add qp build function

--- examples/learned_models/pendulum.jl
+++ examples/learned_models/pendulum.jl
@@ -122,7 +122,7 @@ end
 # Test data
 num_traj_test = 10
 x0_sampler_test = Product([Uniform(-eps(),eps()), Normal(-eps(), eps())])
-initial_conditions_test = tovecs(rand(x0_sampler, num_traj_test), length(x0_sampler))
+initial_conditions_test = tovecs(rand(x0_sampler_test, num_traj_test), length(x0_sampler_test))
 params_sampler_test = PendulumParams(tf=[tf-eps(tf),tf+eps(tf)], QRratio=[1.0, 1.1], Qfratio=[2.0,3.0], u_bnd=[4.0,5.5])
 ctrl_test = ALTROController(genpendulumproblem, params_sampler_test, opts=opts)
 X_test, U_test = create_data(dmodel, ctrl_test, initial_conditions_test, tf, dt)
@@ -177,8 +177,8 @@ U_test = load(datafile, "U_test")
 
 eigfuns = ["state", "sine", "cosine", "hermite"]
 eigorders = [0, 0, 0, 2]
-eigfuns = ["chebyshev"]
-eigorders = [5]
+eigfuns = ["state", "chebyshev"]
+eigorders = [0,5]
 Z_train, Zu_train, kf = build_eigenfunctions(X_train, U_train, eigfuns, eigorders)
 # Z_train, Zu_train, kf = build_eigenfunctions(X_train, U_train, pendulum_kf)
 
@@ -207,18 +207,18 @@ jldsave(joinpath(datadir, "pendulum_eDMD_data.jld2"); A=F, C, g, dt=dt, eigfuns,
 i = 4
 X = X_test[:,i]
 U = U_test[:,i]
-Z = map(x->expandstate(model_bilinear, x), X)
+Y = map(x->expandstate(model_bilinear, x), X)
 times = range(0,tf,step=dt)
 
 # Open loop simulation
-Z_ol = simulate(model_bilinear, U, Z[1], tf, dt)
-X_ol = map(x->originalstate(model_bilinear, x), Z_ol)
+Y_ol = simulate(model_bilinear, U, Y[1], tf, dt)
+X_ol = map(x->originalstate(model_bilinear, x), Y_ol)
 plot(times, reduce(hcat, X)', c=[1 2], label="original")
 plot!(times, reduce(hcat, X_ol)', legend=:bottom, c=[1 2], s=:dash, label="bilinear model (open loop)")
 
 # Closed loop simulation
 using BilinearControl.RandomLinearModels: iscontrollable
-A,B = linearize(model_bilinear, Z, U, times)
+A,B = linearize(model_bilinear, Y, U, times)
 map(zip(A,B)) do (Ak,Bk)
     iscontrollable(Ak,Bk)
 end
@@ -230,18 +230,148 @@ map(1:N-1) do k
     norm(eigvals(A[k] - B[k]*K[k]), Inf)
 end
 
-ctrl_bilinear = TVLQRController(K, Z, U, times)
-Z_cl, = simulatewithcontroller(model_bilinear, ctrl_bilinear, Z[1], tf, dt)
-X_cl = map(x->originalstate(model_bilinear, x), Z_cl)
+ctrl_bilinear = TVLQRController(K, Y, U, times)
+Y_cl, = simulatewithcontroller(model_bilinear, ctrl_bilinear, Y[1], tf, dt)
+X_cl = map(x->originalstate(model_bilinear, x), Y_cl)
 plot(times, reduce(hcat, X)', c=[1 2], label="original")
 plot!(times, reduce(hcat, X_cl)', legend=:bottom, c=[1 2], s=:dash, label="bilinear model")
 
 # Create a controller for the nominal model that uses the bilinear one
 ctrl_bl = BilinearController(model_bilinear, ctrl_bilinear)
-X_bilinear, = simulatewithcontroller(dmodel, ctrl_bl, X[1], tf, dt)
+X_bilinear, = simulatewithcontroller(dmodel, ctrl_bl, X[1] + randn(2)*0e-3, tf, dt)
 p = plot(times, reduce(hcat, X)', c=[1 2], label="original")
 plot!(times, reduce(hcat, X_bilinear)', legend=:bottom, c=[1 2], s=:dash, ylim=[-4,4], label="w/ bilinear TVLQR")
 display(p)
 
 visualize!(vis, model, tf, X)
 visualize!(vis, model, tf, X_bilinear)
+
+## MPC 
+function build_qp!(model::EDMDModel, x0, Q, R, Xref, Uref, times)
+    Yref = map(x->expandstate(model, x), Xref)
+
+    n,m = RD.dims(model)
+    N = length(Xref)
+    Nx = sum(length, Xref)
+    Nu = sum(length, Uref)
+    Ny = sum(length, Yref)
+
+    Ā,B̄ = linearize(model, Yref, Uref, times)
+    d̄ = map(1:N-1) do k
+        dt = times[k+1] - times[k]
+        RD.discrete_dynamics(model, Yref[k], Uref[k], times[k], dt) - (Ā[k]*Yref[k] + B̄[k]*Uref[k])
+    end
+    G = model.g
+
+    # Build Cost
+    P = blockdiag(kron(sparse(I,N,N),G'Q*G), kron(sparse(I,N-1,N-1), R))
+    q0 = mapreduce(vcat, 1:N) do k
+        -G'Q*Xref[k]
+    end
+    r0 = mapreduce(vcat, 1:length(U)) do k
+        -R*Uref[k]
+    end
+    q = [q0; r0]
+    c = map(1:N) do k
+        J = 0.5 * dot(Xref[k], Q, Xref[k])
+        if k < N
+            J += 0.5 * dot(Uref[k], R, Uref[k])
+        end
+        J
+    end
+
+    # Build dynamics
+    D = spzeros(N*n, Ny + Nu) 
+    d = zeros(N*n)
+    ic = 1:n
+    D[ic,1:n] .= -I(n)
+    d[ic] .= -expandstate(model, x0)
+    ic = ic .+ n
+    for k = 1:N-1
+        ix1 = (k-1)*n .+ (1:n)
+        iu1 = (k-1)*m + Ny .+ (1:m)
+        ix2 = ix1 .+ n
+        D[ic,ix1] .= Ā[k]
+        D[ic,iu1] .= B̄[k]
+        D[ic,ix2] .= -I(n)
+        d[ic] .= -d̄[k]
+
+        ic = ic .+ n
+    end
+    A = D
+    ub = d
+    lb = d
+
+    P, q, c, A, lb, ub 
+end
+
+# Generate the QP data
+i = 1
+Q = Diagonal(fill(1.0, n0))
+R = Diagonal(fill(1e-3, m0))
+
+Xref = X_train[:,i]
+Uref = U_train[:,i] 
+Yref = map(x->expandstate(model_bilinear, x), Xref)
+x0 = copy(Xref[i])
+Ny = sum(length, Yref)
+Nu = sum(length, Uref)
+P,q,c, A,lb,ub = build_qp!(model_bilinear, x0, Q, R, Xref, Uref, times)
+
+# Generate random input
+X = [randn(n0) for k = 1:N]
+U = [randn(m0) for k = 1:N-1]
+Y = map(x->expandstate(model_bilinear, x), X)
+
+# Convert to vectors
+x̄ = reduce(vcat, Xref)
+ū = reduce(vcat, Uref)
+ȳ = reduce(vcat, Yref)
+z̄ = [ȳ;ū]
+x = reduce(vcat, X)
+u = reduce(vcat, U)
+y = reduce(vcat, Y)
+z = [y;u]
+
+# Test cost
+J = 0.5 * dot(z, P, z) + dot(q,z) + sum(c)
+J ≈ sum(1:N) do k
+    J = 0.5 * (X[k]-Xref[k])'Q*(X[k]-Xref[k])
+    if k < N
+        J += 0.5 * (U[k] - Uref[k])'R*(U[k] - Uref[k])
+    end
+    J
+end
+
+# Test dynamics constraint
+c = mapreduce(vcat, 1:N-1) do k
+    J = zeros(n,n+m)
+    yn = zeros(n)
+    z̄ = RD.KnotPoint(Yref[k], Uref[k], times[k], dt)
+    RD.jacobian!(RD.InPlace(), RD.UserDefined(), model_bilinear, J, yn, z̄)
+    A = J[:,1:n]
+    B = J[:,n+1:end]
+    dy = Y[k] - Yref[k]
+    du = U[k] - Uref[k]
+    dyn = Y[k+1] - Yref[k+1]
+    RD.discrete_dynamics(model_bilinear, z̄) - Yref[k+1] + A*dy + B*du - dyn
+end
+c = [expandstate(model_bilinear, x0) - Y[1]; c]
+c ≈ Vector(A*z - lb)
+
+import OSQP
+model = OSQP.Model()
+OSQP.setup!(model, P=P, q=q, A=A, l=lb, u=ub)
+res = OSQP.solve!(model)
+zsol = res.x
+ysol = zsol[1:Ny]
+usol = zsol[Ny+1:end]
+Xsol = map(eachcol(reshape(ysol, n, :))) do y
+    originalstate(model_bilinear, y)
+end
+Usol = tovecs(usol, m0)
+plot(times, reduce(hcat,Xref)', c=[1 2], label="reference")
+plot!(times, reduce(hcat,Xsol)', c=[1 2], s=:dash, label="MPC")
+plot(times[1:end-1], reduce(hcat,Uref)', c=1, label="reference")
+plot!(times[1:end-1], reduce(hcat,Usol)', c=1, s=:dash, label="MPC")
+
