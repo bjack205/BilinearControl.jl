69ce2a19ab8324ca69cb6775e47667d2a6cd3c05
Brian Jackson <bjack205@gmail.com>
2022-06-13 10:13:14 -0400

    Got jDMD results

--- examples/cartpole_mistmatch.jl
+++ examples/cartpole_mistmatch.jl
@@ -24,6 +24,7 @@ using ProgressMeter
 
 include("constants.jl")
 include("cartpole_utils.jl")
+const CARTPOLE_MISMATCH_RESULTS = joinpath(Problems.DATADIR, "cartpole_mismatch_results.jld2")
 
 function test_sample_size(;
     μ=0.1,
@@ -128,13 +129,95 @@ function test_sample_size(;
     # end
 end
 
+function find_min_sample_to_stabilize(mu_vals, num_train; 
+        err_thresh=0.1, success_rate_thresh=0.95, repeats_required = 2, verbose=true, 
+        kwargs...
+    )
+    samples_required = Dict(Pair.(mu_vals, zeros(Int,length(mu_vals))))
+    success_counts = Dict(Pair.(mu_vals, zeros(Int,length(mu_vals))))
+    mu_vals_remaining = copy(mu_vals)
+    inds_lock = ReentrantLock()
+
+    for N in num_train
+        inds_to_delete = Int[]
+        verbose && println("Testing with $N samples")
+        Threads.@threads for i = 1:length(mu_vals_remaining) 
+            μ = mu_vals_remaining[i]
+            success_rate, average_error = test_sample_size(;num_train=N, μ, err_thresh, kwargs...)
+            
+            did_successfully_stabilize = (success_rate >= success_rate_thresh) && (average_error < err_thresh)
+            if did_successfully_stabilize
+                verbose && println("  μ = $μ successfully stabilized with $N samples")
+                success_counts[μ] += 1
+            else
+                samples_required[μ] = 0
+                success_counts[μ] = 0
+            end
+            if success_counts[μ] == 1 
+                samples_required[μ] = N 
+            elseif success_counts[μ] > repeats_required
+                verbose && println("  FINISHED: μ = $μ stabilized with $(samples_required[μ]) samples")
+                lock(inds_lock)
+                push!(inds_to_delete, i)
+                unlock(inds_lock)
+            end
+        end
+        deleteat!(mu_vals_remaining, sort!(inds_to_delete))
+        if isempty(mu_vals_remaining)
+            break
+        end
+    end
+    return samples_required
+end
+
 ##
+Threads.nthreads()
 x_window = [1.0, deg2rad(40), 0.5, 0.5]
 test_window_ratio = 0.5
 err_thresh = 0.1
 μ = 0.4
 reg=1e-4
-test_sample_size(; 
-    num_train=80, num_test=20, alg=:eDMD, μ, x_window, test_window_ratio, err_thresh, reg
+num_test = 20
+mu_vals = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+num_train = [2,3,4,5,6,7,8]
+num_train = [2:15; 20:5:100]
+
+α = 0.01
+@time res_jDMD = find_min_sample_to_stabilize(mu_vals, num_train; 
+    num_test=20, alg=:jDMD, x_window, test_window_ratio, reg, α
 )
-test_sample_size(; num_train=10, num_test=20, alg=:jDMD, μ, x_window, test_window_ratio, reg)
+res_jDMD[:α] = 0.01
+res_jDMD
+
+α = 0.5
+@time res_jDMD_2 = find_min_sample_to_stabilize(mu_vals, num_train; 
+    num_test=20, alg=:jDMD, x_window, test_window_ratio, reg, α
+)
+
+α = 0.1
+@time res_jDMD_3 = find_min_sample_to_stabilize(mu_vals, num_train; 
+    num_test=50, alg=:jDMD, x_window, test_window_ratio, reg, α
+)
+
+@time res_eDMD = find_min_sample_to_stabilize(mu_vals, num_train; 
+    num_test=50, alg=:eDMD, x_window, test_window_ratio, reg, α
+)
+
+res_jDMD_all = [
+    merge(res_jDMD, Dict(:α=>0.01)),
+    merge(res_jDMD_3, Dict(:α=>0.1)),
+    merge(res_jDMD_2, Dict(:α=>0.5)),
+]
+jldsave(CARTPOLE_MISMATCH_RESULTS; res_jDMD = res_jDMD_all)
+
+@time res_jDMD = find_min_sample_to_stabilize(mu_vals, num_train; 
+    num_test=20, alg=:jDMD, x_window, test_window_ratio, reg, α
+)
+
+success_rate, average_error = test_sample_size(;num_train=9, alg=:jDMD, μ=0.5,
+    num_test=50, err_thresh, reg, x_window, test_window_ratio, α=0.1
+)
+
+success_rate, average_error = test_sample_size(;num_train=100, alg=:eDMD, μ=0.4,
+    num_test=50, err_thresh, reg=1e-4, x_window, test_window_ratio, α=0.1
+)
