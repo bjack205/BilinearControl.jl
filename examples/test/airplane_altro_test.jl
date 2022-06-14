134ddc9fd7ae74369f9d7c1195e8826ebedfc448
Brian Jackson <bjack205@gmail.com>
2022-06-14 15:10:09 -0400

    cant' get mpc to work...

--- examples/test/airplane_altro_test.jl
+++ examples/test/airplane_altro_test.jl
@@ -12,6 +12,7 @@ using RobotDynamics
 using TrajectoryOptimization
 const TO = TrajectoryOptimization
 import RobotDynamics as RD
+include("airplane_problem.jl")
 
 ## Visualizer
 model = Problems.NominalAirplane()
@@ -22,8 +23,11 @@ set_airplane!(vis, model)
 open(vis)
 
 ##
+model_nom = Problems.NominalAirplane()
+model_real = Problems.SimulatedAirplane()
+dmodel_nom = RD.DiscretizedDynamics{RD.RK4}(model_nom)
+dmodel_real = RD.DiscretizedDynamics{RD.RK4}(model_real)
 
-##
 tf = 2.0
 dt = 0.05
 prob = AirplaneProblem(;tf, dt)
@@ -33,18 +37,12 @@ Altro.cost(prob)
 solver = ALTROSolver(prob, verbose=4)
 solve!(solver)
 visualize!(vis, model_real, TO.get_final_time(prob), states(solver))
-states(solver)[end][7]
 X_ref = Vector.(states(solver))
 U_ref = push!(Vector.(controls(solver)), u_trim)
 T_ref = TO.gettimes(solver)
 norm(X_ref[end][7:9])
 
 ## Simulate open-loop with real dynamics
-model_nom = Problems.NominalAirplane()
-model_real = Problems.SimulatedAirplane()
-dmodel_nom = RD.DiscretizedDynamics{RD.RK4}(model_nom)
-dmodel_real = RD.DiscretizedDynamics{RD.RK4}(model_real)
-
 X_real, = simulate(dmodel_real, U_ref, X_ref[1], tf, dt)
 visualize!(vis, model_real, tf, X_real)
 
@@ -52,7 +50,7 @@ visualize!(vis, model_real, tf, X_real)
 Qk = Diagonal([fill(1e-0, 3); fill(1e-0, 3); fill(1e-1, 3); fill(1e-1, 3)])
 Rk = Diagonal(fill(1e-3,4))
 Qf = 10 * copy(Qk)
-mpc = TrackingMPC(dmodel_nom, X_ref, U_ref, T_ref, Qk, Rk, Qf, Nt=21)
+mpc = EDMD.TrackingMPC(dmodel_nom, X_ref, U_ref, T_ref, Qk, Rk, Qf, Nt=21)
 EDMD.getcontrol(mpc, X_ref[1], 0.0)
 X_sim, = simulatewithcontroller(dmodel_real, mpc, X_ref[1], tf, dt)
 visualize!(vis, model_real, tf, X_sim)
