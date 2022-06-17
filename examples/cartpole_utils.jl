6785965351afcf8e468fdbb2d8621a626e574e92
Brian Jackson <bjack205@gmail.com>
2022-06-17 15:11:46 -0400

    Add more cartpole tests

--- examples/cartpole_utils.jl
+++ examples/cartpole_utils.jl
@@ -4,6 +4,8 @@ using LinearAlgebra
 import RobotDynamics as RD
 using StaticArrays
 using Statistics
+using Distributions
+using Random
 using JLD2
 
 function gencartpoleproblem(x0=zeros(4), Qv=1e-2, Rv=1e-1, Qfv=1e2, u_bnd=3.0, tf=5.0; 
@@ -363,4 +365,248 @@ function train_cartpole_models(num_lqr, num_swingup; α=0.5, learnB=true, β=1.0
     (;nom_err_avg, eDMD_err_avg, eDMD_success, jDMD_err_avg, jDMD_success, 
         t_train_eDMD, t_train_jDMD, num_lqr, num_swingup, nsamples=length(X_train), 
         )
+end
+
+function test_sample_size(;
+    μ=0.1,
+    μ_nom=μ,
+    t_sim=4.0,
+    num_train=20,
+    num_test=10,
+    err_thresh=0.1,
+    alg=:eDMD,
+    α=1e-1,
+    reg=1e-6,
+    x_window=[1, deg2rad(40), 0.5, 0.5],
+    test_window_ratio=1.0,
+    lifted=false,
+    ρ=1e-6,
+    Nt=21,
+)
+
+    ## Define the models
+    model_nom = Problems.NominalCartpole()
+    dmodel_nom = RD.DiscretizedDynamics{RD.RK4}(model_nom)
+
+    # Define Mismatched "Real" Cartpole Model
+    model_real = Problems.SimulatedCartpole(; μ=μ) # this model has damping
+    dmodel_real = RD.DiscretizedDynamics{RD.RK4}(model_real)
+
+    # Generate data with the new damping term
+    X_train, U_train, _, _, _, _, metadata = generate_cartpole_data(;
+        save_to_file=false,
+        num_swingup=0,
+        num_lqr=num_train,
+        μ=μ,
+        μ_nom=μ_nom,
+        max_lqr_samples=600,
+        x_window,
+    )
+    dt = metadata.dt
+
+    # Train new model
+    eigfuns = ["state", "sine", "cosine", "sine", "sine", "chebyshev"]
+    eigorders = [[0], [1], [1], [2], [4], [2, 4]]
+
+    model = if alg == :eDMD
+        run_eDMD(
+            X_train,
+            U_train,
+            dt,
+            eigfuns,
+            eigorders;
+            reg=reg,
+            name="cartpole_eDMD",
+            alg=:qr_rls,
+        )
+    elseif alg == :jDMD
+        run_jDMD(
+            X_train,
+            U_train,
+            dt,
+            eigfuns,
+            eigorders,
+            dmodel_nom;
+            reg=reg,
+            name="cartpole_jDMD",
+            learnB=true,
+            α=α,
+        )
+    end
+
+    ## Generate an MPC controller
+    mpc = if lifted
+        generate_stabilizing_mpc_controller(model, t_sim, dt; Nt, ρ)
+    else
+        model_projected = EDMD.ProjectedEDMDModel(model)
+        generate_stabilizing_mpc_controller(model_projected, t_sim, dt; Nt, ρ)
+    end
+    return test_initial_conditions(
+        dmodel_real, mpc, dt; t_sim, x_window, test_window_ratio, num_test, err_thresh
+    )
+end
+
+function test_initial_conditions(
+    model_real,
+    ctrl,
+    dt;
+    t_sim=4.0,
+    x_window=[1, deg2rad(30), 0.5, 0.5],
+    test_window_ratio=1.0,
+    num_test=50,
+    err_thresh=0.1,
+)
+
+    # Test mpc controller
+    # Set seed so that all are tested on the same conditions
+    Random.seed!(100)
+
+    # Generate initial conditions to test
+    xe = [0, pi, 0, 0]
+    x0_sampler = Product(
+        collect(
+            Uniform(x - dx, x + dx) for (x, dx) in zip(xe, x_window .* test_window_ratio)
+        ),
+    )
+    x0_test = [rand(x0_sampler) for i in 1:num_test]
+
+    # Run controller for each initial condition
+    errors = map(x0_test) do x0
+        X_sim, = simulatewithcontroller(model_real, ctrl, x0, t_sim, dt)
+        return norm(X_sim[end] - xe)
+    end
+    average_error = mean(filter(x -> x < err_thresh, errors))
+    success_rate = count(x -> x < err_thresh, errors) / num_test
+    return success_rate, average_error
+end
+
+function find_min_sample_to_stabilize(
+    mu_vals,
+    num_train;
+    err_thresh=0.1,
+    success_rate_thresh=0.95,
+    repeats_required=2,
+    verbose=true,
+    kwargs...,
+)
+    samples_required = Dict(Pair.(mu_vals, zeros(Int, length(mu_vals))))
+    success_counts = Dict(Pair.(mu_vals, zeros(Int, length(mu_vals))))
+    mu_vals_remaining = copy(collect(mu_vals))
+    inds_lock = ReentrantLock()
+
+    for N in num_train
+        inds_to_delete = Int[]
+        verbose && println("Testing with $N samples")
+        Threads.@threads for i in 1:length(mu_vals_remaining)
+            μ = mu_vals_remaining[i]
+            success_rate, average_error = test_sample_size(;
+                num_train=N, μ, err_thresh, kwargs...
+            )
+
+            did_successfully_stabilize =
+                (success_rate >= success_rate_thresh) && (average_error < err_thresh)
+            if did_successfully_stabilize
+                verbose && println("  μ = $μ successfully stabilized with $N samples")
+                success_counts[μ] += 1
+            else
+                samples_required[μ] = 0
+                success_counts[μ] = 0
+            end
+            if success_counts[μ] == 1
+                samples_required[μ] = N
+            elseif success_counts[μ] > repeats_required
+                verbose && println(
+                    "  FINISHED: μ = $μ stabilized with $(samples_required[μ]) samples"
+                )
+                lock(inds_lock)
+                push!(inds_to_delete, i)
+                unlock(inds_lock)
+            end
+        end
+        deleteat!(mu_vals_remaining, sort!(inds_to_delete))
+        if isempty(mu_vals_remaining)
+            break
+        end
+    end
+    return samples_required
+end
+
+"""
+    find_min_sample_to_beat_mpc(num_train, dt; kwargs)
+
+Find the number of samples required be the nominal MPC controller at the cartpole 
+stabilization task, using MPC controller.
+
+setting `lifted = true`.
+
+# Keyword Arguments
+- `alg` algorithm to use (:eDMD, :jDMD)
+- `lifted` used the lifted MPC controller
+- `num_test` number of test initial conditions to test`
+- `repeats_required` number of successive sequential sample sizes that need to succesfully
+   stabilize the system for the sample size to be considered the minimum number of samples.
+   Prevents counting extranneous samples sizes that stabilize but larger sample sizes don't.
+- `x_window` Vector of widths from which to sample the initial conditions
+- `test_window_ratio` Ratio of `x_window` over which to draw the test initial conditions. A 
+   value of 1.0 samples from the same distribution. A lower value is more conservative, a 
+   value greater than 1.0 means samples may be drawn that are not in the training 
+   distribution.
+- `t_sim` How long to run the simulation. Trajectories have a final state value less than 
+   `error_thresh` to be considered successful.
+- `Nt` MPC horizon
+"""
+function find_min_sample_to_beat_mpc(
+    num_train,
+    dt;
+    num_test=50,
+    repeats_required=2,
+    success_rate_thresh=0.95,
+    x_window=[1, deg2rad(40), 0.5, 0.5],
+    test_window_ratio=0.9,  # chosen to give MPC controller a 98% success rate
+    t_sim=4.0,
+    Nt=21,
+    verbose=true,
+    kwargs...,
+)
+    samples_required = 0
+    success_counts = 0 
+    mu_vals_remaining = 0 
+
+    ## Get the nominal MPC performance
+    dmodel_nom = RD.DiscretizedDynamics{RD.RK4}(Problems.NominalCartpole())
+    dmodel_real = RD.DiscretizedDynamics{RD.RK4}(Problems.SimulatedCartpole())
+    mpc_nom = generate_stabilizing_mpc_controller(dmodel_nom, t_sim, dt; Nt)
+    success_rate, err_mpc_nom = test_initial_conditions(
+        dmodel_real, mpc_nom, dt; x_window, test_window_ratio, num_test
+    )
+    verbose && println("Nominal MPC error: ", err_mpc_nom)
+    verbose && println("Nominal MPC success rate: ", success_rate)
+
+    for N in num_train
+        inds_to_delete = Int[]
+        verbose && println("Testing with $N samples. Count = ", success_counts, " / ", repeats_required + 1)
+        success_rate, average_error = test_sample_size(; 
+            num_train=N, num_test=50, μ_nom=0.0, x_window, test_window_ratio, t_sim, kwargs...
+        )
+        verbose && println("  got (", success_rate, ", ", average_error, ")")
+
+        did_successfully_stabilize =
+            (success_rate >= success_rate_thresh) && (average_error < err_mpc_nom)
+        if did_successfully_stabilize
+            verbose && println("  successfully stabilized with $N samples")
+            success_counts += 1
+        else
+            samples_required = 0
+            success_counts = 0
+        end
+        if success_counts == 1
+            samples_required = N
+        elseif success_counts > repeats_required
+            verbose && println(
+                "  FINISHED: stabilized with $(samples_required) samples"
+            )
+            break
+        end
+    end
+    return samples_required
 end
--- test/cartpole_tests.jl
+++ test/cartpole_tests.jl
@@ -1,9 +1,13 @@
 
 using BilinearControl
 using BilinearControl.EDMD
+using BilinearControl.Problems
+using BilinearControl: Problems
 using Test
 include(joinpath(BilinearControl.EXAMPLES_DIR,"cartpole_utils.jl"))
 
+## Test Cartpole controller at specific sample size
+println("\nTESTING CARTPOLE MPC CONTROLLER WITH N = ", 2)
 const CARTPOLE_RESULTS_FILE = joinpath(Problems.DATADIR, "cartpole_results.jld2")
 res = train_cartpole_models(0,2, α=0.5, β=1.0, learnB=true, reg=1e-4)
 @test abs(res.nom_err_avg - 0.1563) < 1e-4
@@ -12,6 +16,7 @@ res = train_cartpole_models(0,2, α=0.5, β=1.0, learnB=true, reg=1e-4)
 @test res.num_swingup == 2
 @test res.jDMD_success == 7
 
+println("\nTESTING CARTPOLE MPC CONTROLLER WITH N = ", 10)
 res = train_cartpole_models(0,10, α=0.5, β=1.0, learnB=true, reg=1e-4)
 @test abs(res.nom_err_avg - 0.1563) < 1e-4
 @test abs(res.eDMD_err_avg - 0.096449) < 1e-4
@@ -19,3 +24,34 @@ res = train_cartpole_models(0,10, α=0.5, β=1.0, learnB=true, reg=1e-4)
 @test res.num_swingup == 10 
 @test res.jDMD_success == 7
 
+## Test cartpole mismatch
+test_window_ratio = 0.5
+reg = 1e-4
+num_train = [2:25; 25:5:100]
+mu_vals = 0:0.1:0.6
+num_test = 50
+repeats_required = 4
+
+α = 0.01
+println("\nTESTING CARTPOLE MISMATCH WITH α = ", α)
+@time res_jDMD = find_min_sample_to_stabilize(
+    mu_vals, num_train; num_test, alg=:jDMD, test_window_ratio, reg, α,
+    repeats_required
+)
+vals = map(mu->res_jDMD[mu], mu_vals)
+@test vals == [2,2,2,2,3,7,12]
+
+## Compare projected and lifted MPC
+dt = 0.05
+repeates_required = 4
+println("FINDING MIN SAMPLES FOR PROJECTED MPC")
+res_jDMD = find_min_sample_to_beat_mpc(2:10, dt; alg=:jDMD, lifted=false, 
+    repeats_required, α=0.1
+)
+@test res_jDMD == 2
+
+println("FINDING MIN SAMPLES FOR LIFTED MPC")
+res_jDMD_lifted = find_min_sample_to_beat_mpc(13:25, dt; alg=:jDMD, lifted=true, 
+    repeats_required, α=0.1
+)
+res_jDMD_lifted == 15
