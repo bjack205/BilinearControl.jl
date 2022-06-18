33a1e700a41809dc7d6f41b18d9fc3a203b05fbc
Brian Jackson <bjack205@gmail.com>
2022-06-18 17:06:47 -0400

    Delete odd files in examples

--- examples/visualization/visualization.jl
+++ examples/visualization/visualization.jl
@@ -7,47 +7,6 @@ using RobotZoo
 import RobotDynamics as RD
 import BilinearControl.Problems: orientation, translation
 
-#############################################
-# Endurance (satellite) 
-#############################################
-
-"""
-    setbackgroundimage(vis, imagefile, [d])
-
-Set an image as a pseudo-background image by placing an image on planes located a distance
-`d` from the origin.
-"""
-function setbackgroundimage(vis, imagefile, d=50)
-
-    # Create planes at a distance d from the origin
-    px = Rect3D(Vec(+d,-d,-d), Vec(0.1,2d,2d))
-    nx = Rect3D(Vec(-d,-d,-d), Vec(0.1,2d,2d))
-    py = Rect3D(Vec(-d,+d,-d), Vec(2d,0.1,2d))
-    ny = Rect3D(Vec(-d,-d,-d), Vec(2d,0.1,2d))
-    pz = Rect3D(Vec(-d,-d,+d), Vec(2d,2d,0.1))
-    nz = Rect3D(Vec(-d,-d,-d), Vec(2d,2d,0.1))
-
-    # Create a material from the image
-    img = PngImage(imagefile)
-    mat = MeshLambertMaterial(map=Texture(image=img))
-
-    # Set the objects
-    setobject!(vis["background"]["px"], px, mat)
-    setobject!(vis["background"]["nx"], nx, mat)
-    setobject!(vis["background"]["py"], py, mat)
-    setobject!(vis["background"]["ny"], ny, mat)
-    setobject!(vis["background"]["pz"], pz, mat)
-    setobject!(vis["background"]["nz"], nz, mat)
-    return vis 
-end
-
-function setendurance!(vis; scale=1/3)
-    meshfile = joinpath(@__DIR__, "Endurance and ranger.obj")
-    obj = MeshFileObject(meshfile)
-    setbackgroundimage(vis, joinpath(@__DIR__, "stars.jpg"), 30)
-    setobject!(vis["robot"]["geometry"], obj)
-    settransform!(vis["robot"]["geometry"], compose(Translation((5.05, 6.38, 1) .* scale), LinearMap(I*scale * RotZ(deg2rad(140)))))
-end
 
 #############################################
 # Quadrotor 
