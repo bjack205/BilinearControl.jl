11f9058079ffc6fc4741b57a22732cd73c060b7b
Brian Jackson <bjack205@gmail.com>
2022-08-26 13:54:32 -0400

    Update airplane plots

--- examples/airplane/airplane_main.jl
+++ examples/airplane/airplane_main.jl
@@ -116,9 +116,13 @@ err_jDMD = get_median_error(results, :jDMD)
 quant_min_nom, quant_max_nom  = get_error_quantile(results, :nominal) 
 quant_min_eDMD, quant_max_eDMD = get_error_quantile(results, :eDMD) 
 quant_min_jDMD, quant_max_jDMD = get_error_quantile(results, :jDMD) 
+
 jerr_nom  = get_average_error(results, :jerr_nominal) 
 jerr_eDMD = get_average_error(results, :jerr_eDMD) 
 jerr_jDMD = get_average_error(results, :jerr_jDMD) 
+jerr_nom_lo, jerr_nom_up = get_error_quantile(results, :jerr_nominal) 
+jerr_eDMD_lo, jerr_eDMD_up = get_error_quantile(results, :jerr_eDMD) 
+jerr_jDMD_lo, jerr_jDMD_up = get_error_quantile(results, :jerr_jDMD) 
 
 sr_nom  = get_success_rate(results, :nominal) 
 sr_eDMD = get_success_rate(results, :eDMD) 
@@ -136,6 +140,16 @@ invalidate_by_success_rate!(quant_max_nom, sr_nom)
 invalidate_by_success_rate!(quant_max_eDMD, sr_eDMD)
 invalidate_by_success_rate!(quant_max_jDMD, sr_jDMD)
 
+invalidate_by_success_rate!(jerr_nom, sr_nom)
+invalidate_by_success_rate!(jerr_nom_up, sr_nom)
+invalidate_by_success_rate!(jerr_nom_lo, sr_nom)
+invalidate_by_success_rate!(jerr_eDMD, sr_eDMD)
+invalidate_by_success_rate!(jerr_eDMD_up, sr_eDMD)
+invalidate_by_success_rate!(jerr_eDMD_lo, sr_eDMD)
+invalidate_by_success_rate!(jerr_jDMD, sr_jDMD)
+invalidate_by_success_rate!(jerr_jDMD_up, sr_jDMD)
+invalidate_by_success_rate!(jerr_jDMD_lo, sr_jDMD)
+
 p_err = @pgf Axis(
     {
         xmajorgrids,
@@ -144,32 +158,65 @@ p_err = @pgf Axis(
         xlabel = "Number of Training Trajectories",
         ylabel = "Tracking Error",
         legend_style = "{at={(0.97,0.7)},anchor=east}",
-        # ymax = 0.11,
+        ymax = 0.22,
     },
 
     PlotInc({lineopts..., color=color_nominal, solid, thick}, Coordinates(num_train, err_nom)),
 
-    # PlotInc({lineopts..., "name path=A", "black!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, quant_min_nom)),
-    # PlotInc({lineopts..., "name_path=B", "black!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, quant_max_nom)),
-    # PlotInc({lineopts..., "black!10", "forget plot"}, "fill between [of=A and B]"),
+    PlotInc({lineopts..., "name path=A", "black!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, quant_min_nom)),
+    PlotInc({lineopts..., "name_path=B", "black!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, quant_max_nom)),
+    PlotInc({lineopts..., "black!10", "forget plot"}, "fill between [of=A and B]"),
 
     PlotInc({lineopts..., color=color_eDMD, solid, thick}, Coordinates(num_train, err_eDMD)),
     
-    # PlotInc({lineopts..., "name_path=C", "orange!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, quant_min_eDMD)),
-    # PlotInc({lineopts..., "name_path=D", "orange!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, quant_max_eDMD)),
-    # PlotInc({lineopts..., "orange!10", "forget plot"}, "fill between [of=C and D]"),
+    PlotInc({lineopts..., "name_path=C", "orange!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, quant_min_eDMD)),
+    PlotInc({lineopts..., "name_path=D", "orange!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, quant_max_eDMD)),
+    PlotInc({lineopts..., "orange!10", "forget plot"}, "fill between [of=C and D]"),
 
     PlotInc({lineopts..., color=color_jDMD, solid, thick}, Coordinates(num_train, err_jDMD)),
-    # PlotInc({lineopts..., "name_path=E", "cyan!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, quant_min_jDMD)),
-    # PlotInc({lineopts..., "name_path=F","cyan!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, quant_max_jDMD)),
-    # PlotInc({lineopts..., "cyan!10", "forget plot"}, "fill between [of=E and F]"),
+    PlotInc({lineopts..., "name_path=E", "cyan!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, quant_min_jDMD)),
+    PlotInc({lineopts..., "name_path=F","cyan!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, quant_max_jDMD)),
+    PlotInc({lineopts..., "cyan!10", "forget plot"}, "fill between [of=E and F]"),
 
     Legend(["Nominal MPC", "EDMD", "JDMD"])
 
-)
+);
 pgfsave(joinpath(BilinearControl.FIGDIR, "airplane_error_by_num_train.tikz"), p_err, 
     include_preamble=false)
 
+p_jerr = @pgf Axis(
+    {
+        xmajorgrids,
+        ymajorgrids,
+        ymode="log",
+        xlabel = "Number of Training Trajectories",
+        ylabel = "Jacobian Error",
+        # legend_style = "{at={(0.97,0.7)},anchor=east}",
+        "legend_pos" = "north east",
+        # ymax = 0.11,
+    },
+
+    PlotInc({lineopts..., color=color_nominal, solid, thick}, Coordinates(num_train, jerr_nom)),
+
+    PlotInc({lineopts..., "name path=A", "black!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, jerr_nom_lo)),
+    PlotInc({lineopts..., "name_path=B", "black!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, jerr_nom_up)),
+    PlotInc({lineopts..., "black!10", "forget plot"}, "fill between [of=A and B]"),
+
+    PlotInc({lineopts..., color=color_eDMD, solid, thick}, Coordinates(num_train, jerr_eDMD)),
+    
+    PlotInc({lineopts..., "name_path=C", "orange!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, jerr_eDMD_lo)),
+    PlotInc({lineopts..., "name_path=D", "orange!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, jerr_eDMD_up)),
+    PlotInc({lineopts..., "orange!10", "forget plot"}, "fill between [of=C and D]"),
+
+    PlotInc({lineopts..., color=color_jDMD, solid, thick}, Coordinates(num_train, jerr_jDMD)),
+    PlotInc({lineopts..., "name_path=E", "cyan!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, jerr_jDMD_lo)),
+    PlotInc({lineopts..., "name_path=F", "cyan!10", "forget plot", solid, line_width=0.1}, Coordinates(num_train, jerr_jDMD_up)),
+    PlotInc({lineopts..., "cyan!10", "forget plot"}, "fill between [of=E and F]"),
+
+    Legend(["Nominal", "EDMD", "JDMD"])
+);
+pgfsave(joinpath(BilinearControl.FIGDIR, "airplane_jacobian_error.tikz"), p_jerr, 
+    include_preamble=false)
 #############################################
 ## Model Prediction Error 
 #############################################
@@ -224,43 +271,43 @@ jdmd_err_ol_filtered = invalidate_by_success_rate!(copy(jdmd_err_ol), jdmd_sr_ol
 jdmd_err_cl_filtered = invalidate_by_success_rate!(copy(jdmd_err_cl), jdmd_sr_cl, thresh=0.9)
 
 # prediction error
-p_err = @pgf Axis(
-    {
-        xmajorgrids,
-        ymajorgrids,
-        ymode="log",
-        xlabel = L"\alpha",
-        ylabel = "Error",
-        legend_pos = "north west",
-    },
+# p_err = @pgf Axis(
+#     {
+#         xmajorgrids,
+#         ymajorgrids,
+#         ymode="log",
+#         xlabel = L"\alpha",
+#         ylabel = "Error",
+#         legend_pos = "north west",
+#     },
 
-    PlotInc({lineopts..., "name_path=E", "orange!10", "forget plot", solid, line_width=0.1}, Coordinates(alpha, edmd_quant_min_ol)),
-    PlotInc({lineopts..., "name_path=F","orange!10", "forget plot", solid, line_width=0.1}, Coordinates(alpha, edmd_quant_max_ol)),
-    PlotInc({lineopts..., "orange!10", "forget plot"}, "fill between [of=E and F]"),
+#     PlotInc({lineopts..., "name_path=E", "orange!10", "forget plot", solid, line_width=0.1}, Coordinates(alpha, edmd_quant_min_ol)),
+#     PlotInc({lineopts..., "name_path=F","orange!10", "forget plot", solid, line_width=0.1}, Coordinates(alpha, edmd_quant_max_ol)),
+#     PlotInc({lineopts..., "orange!10", "forget plot"}, "fill between [of=E and F]"),
 
-    PlotInc({lineopts..., "name_path=G", "cyan!10", "forget plot", solid, line_width=0.1}, Coordinates(alpha, jdmd_quant_min_ol)),
-    PlotInc({lineopts..., "name_path=H","cyan!10", "forget plot", solid, line_width=0.1}, Coordinates(alpha, jdmd_quant_max_ol)),
-    PlotInc({lineopts..., "cyan!10", "forget plot"}, "fill between [of=G and H]"),
+#     PlotInc({lineopts..., "name_path=G", "cyan!10", "forget plot", solid, line_width=0.1}, Coordinates(alpha, jdmd_quant_min_ol)),
+#     PlotInc({lineopts..., "name_path=H","cyan!10", "forget plot", solid, line_width=0.1}, Coordinates(alpha, jdmd_quant_max_ol)),
+#     PlotInc({lineopts..., "cyan!10", "forget plot"}, "fill between [of=G and H]"),
 
-    PlotInc({lineopts..., "name_path=A", "orange!20", "forget plot", solid, line_width=0.1}, Coordinates(alpha, edmd_quant_min_cl)),
-    PlotInc({lineopts..., "name_path=B","orange!20", "forget plot", solid, line_width=0.1}, Coordinates(alpha, edmd_quant_max_cl)),
-    PlotInc({lineopts..., "orange!20", "forget plot"}, "fill between [of=A and B]"),
+#     PlotInc({lineopts..., "name_path=A", "orange!20", "forget plot", solid, line_width=0.1}, Coordinates(alpha, edmd_quant_min_cl)),
+#     PlotInc({lineopts..., "name_path=B","orange!20", "forget plot", solid, line_width=0.1}, Coordinates(alpha, edmd_quant_max_cl)),
+#     PlotInc({lineopts..., "orange!20", "forget plot"}, "fill between [of=A and B]"),
 
-    PlotInc({lineopts..., "name_path=C", "cyan!20", "forget plot", solid, line_width=0.1}, Coordinates(alpha, jdmd_quant_min_cl)),
-    PlotInc({lineopts..., "name_path=D","cyan!20", "forget plot", solid, line_width=0.1}, Coordinates(alpha, jdmd_quant_max_cl)),
-    PlotInc({lineopts..., "cyan!20", "forget plot"}, "fill between [of=C and D]"),
+#     PlotInc({lineopts..., "name_path=C", "cyan!20", "forget plot", solid, line_width=0.1}, Coordinates(alpha, jdmd_quant_min_cl)),
+#     PlotInc({lineopts..., "name_path=D","cyan!20", "forget plot", solid, line_width=0.1}, Coordinates(alpha, jdmd_quant_max_cl)),
+#     PlotInc({lineopts..., "cyan!20", "forget plot"}, "fill between [of=C and D]"),
 
-    PlotInc({lineopts..., color=color_eDMD, solid, thick}, Coordinates(alpha, edmd_err_cl)),
-    PlotInc({lineopts..., "orange!50", dashed, thick}, Coordinates(alpha, edmd_err_ol)),
-    PlotInc({lineopts..., color=color_jDMD, solid, thick}, Coordinates(alpha, jdmd_err_cl)),
-    PlotInc({lineopts..., "cyan!50", dashed, thick}, Coordinates(alpha, jdmd_err_ol)),
+#     PlotInc({lineopts..., color=color_eDMD, solid, thick}, Coordinates(alpha, edmd_err_cl)),
+#     PlotInc({lineopts..., "orange!50", dashed, thick}, Coordinates(alpha, edmd_err_ol)),
+#     PlotInc({lineopts..., color=color_jDMD, solid, thick}, Coordinates(alpha, jdmd_err_cl)),
+#     PlotInc({lineopts..., "cyan!50", dashed, thick}, Coordinates(alpha, jdmd_err_ol)),
 
-    # Legend(["Closed-loop", "Open-loop"])
+#     # Legend(["Closed-loop", "Open-loop"])
 
-)
+# )
 
-pgfsave(joinpath(BilinearControl.FIGDIR, "airplane_model_error_by_alpha.tikz"), p_err, 
-    include_preamble=false)
+# pgfsave(joinpath(BilinearControl.FIGDIR, "airplane_model_error_by_alpha.tikz"), p_err, 
+#     include_preamble=false)
 
 # prediction error just jdmd
 
@@ -274,12 +321,12 @@ p_err = @pgf Axis(
         legend_pos = "north west",
     },
 
-    PlotInc({lineopts..., color=color_jDMD, solid, thick}, Coordinates(alpha, jdmd_err_cl_filtered)),
+    PlotInc({lineopts..., color=color_jDMD, solid, thick}, Coordinates(alpha, jdmd_err_cl)),
     PlotInc({lineopts..., "name_path=E", "cyan!20", "forget plot", solid, line_width=0.1}, Coordinates(alpha, jdmd_quant_min_cl)),
     PlotInc({lineopts..., "name_path=F","cyan!20", "forget plot", solid, line_width=0.1}, Coordinates(alpha, jdmd_quant_max_cl)),
     PlotInc({lineopts..., "cyan!20", "forget plot"}, "fill between [of=E and F]"),
 
-    PlotInc({lineopts..., "cyan!50", dashed, thick}, Coordinates(alpha, jdmd_err_ol_filtered)),
+    PlotInc({lineopts..., "cyan!50", dashed, thick}, Coordinates(alpha, jdmd_err_ol)),
     PlotInc({lineopts..., "name_path=E", "cyan!10", "forget plot", solid, line_width=0.1}, Coordinates(alpha, jdmd_quant_min_ol)),
     PlotInc({lineopts..., "name_path=F","cyan!10", "forget plot", solid, line_width=0.1}, Coordinates(alpha, jdmd_quant_max_ol)),
     PlotInc({lineopts..., "cyan!10", "forget plot"}, "fill between [of=E and F]"),
@@ -291,59 +338,59 @@ p_err = @pgf Axis(
 pgfsave(joinpath(BilinearControl.FIGDIR, "airplane_model_error_by_alpha_just_jdmd.tikz"), p_err, 
     include_preamble=false)
 
-# success rate
-p_err = @pgf Axis(
-    {
-        xmajorgrids,
-        ymajorgrids,
-        xlabel = L"\alpha",
-        ylabel = "Success Rate",
-        legend_pos = "south west",
-    },
+# # success rate
+# p_err = @pgf Axis(
+#     {
+#         xmajorgrids,
+#         ymajorgrids,
+#         xlabel = L"\alpha",
+#         ylabel = "Success Rate",
+#         legend_pos = "south west",
+#     },
 
-    PlotInc({lineopts..., color=color_eDMD, solid, thick}, Coordinates(alpha, edmd_sr_cl)),
-    PlotInc({lineopts..., "orange!50", dashed, thick}, Coordinates(alpha, edmd_sr_ol)),
+#     PlotInc({lineopts..., color=color_eDMD, solid, thick}, Coordinates(alpha, edmd_sr_cl)),
+#     PlotInc({lineopts..., "orange!50", dashed, thick}, Coordinates(alpha, edmd_sr_ol)),
 
-    PlotInc({lineopts..., color=color_jDMD, solid, thick}, Coordinates(alpha, jdmd_sr_cl)),
-    PlotInc({lineopts..., "cyan!50", dashed, thick}, Coordinates(alpha, jdmd_sr_ol)),
-    Legend(["Closed-loop (EDMD)", "Open-loop (EDMD)", "Closed-loop (JDMD)", "Open-loop (JDMD)"])
+#     PlotInc({lineopts..., color=color_jDMD, solid, thick}, Coordinates(alpha, jdmd_sr_cl)),
+#     PlotInc({lineopts..., "cyan!50", dashed, thick}, Coordinates(alpha, jdmd_sr_ol)),
+#     Legend(["Closed-loop (EDMD)", "Open-loop (EDMD)", "Closed-loop (JDMD)", "Open-loop (JDMD)"])
 
-)
-pgfsave(joinpath(BilinearControl.FIGDIR, "airplane_success_rate_by_alpha.tikz"), p_err, 
-    include_preamble=false)
+# )
+# pgfsave(joinpath(BilinearControl.FIGDIR, "airplane_success_rate_by_alpha.tikz"), p_err, 
+#     include_preamble=false)
 
-# success rate just jdmd
-p_err = @pgf Axis(
-    {
-        xmajorgrids,
-        ymajorgrids,
-        xlabel = L"\alpha",
-        ylabel = "Success Rate",
-        legend_pos = "south west",
-    },
+# # success rate just jdmd
+# p_err = @pgf Axis(
+#     {
+#         xmajorgrids,
+#         ymajorgrids,
+#         xlabel = L"\alpha",
+#         ylabel = "Success Rate",
+#         legend_pos = "south west",
+#     },
 
-    PlotInc({lineopts..., color=color_jDMD, solid, thick}, Coordinates(alpha, jdmd_sr_cl)),
-    PlotInc({lineopts..., "cyan!50", dashed, thick}, Coordinates(alpha, jdmd_sr_ol)),
-    Legend(["Closed-loop (JDMD)", "Open-loop (JDMD)"])
+#     PlotInc({lineopts..., color=color_jDMD, solid, thick}, Coordinates(alpha, jdmd_sr_cl)),
+#     PlotInc({lineopts..., "cyan!50", dashed, thick}, Coordinates(alpha, jdmd_sr_ol)),
+#     Legend(["Closed-loop (JDMD)", "Open-loop (JDMD)"])
 
-)
-pgfsave(joinpath(BilinearControl.FIGDIR, "airplane_success_rate_by_alpha_just_jdmd.tikz"), p_err, 
-    include_preamble=false)
+# )
+# pgfsave(joinpath(BilinearControl.FIGDIR, "airplane_success_rate_by_alpha_just_jdmd.tikz"), p_err, 
+#     include_preamble=false)
 
-p_jerr = @pgf Axis(
-    {
-        xmajorgrids,
-        ymajorgrids,
-        # ymode="log",
-        xlabel = "Number of training trajectories",
-        ylabel = "Jacobian Error",
-        legend_style="{at={(0.97,0.65)},anchor=east}"
-    },
-    PlotInc({lineopts..., color=color_nominal}, Coordinates(num_train, jerr_nom)),
-    PlotInc({lineopts..., color=color_eDMD}, Coordinates(num_train, jerr_eDMD)),
-    PlotInc({lineopts..., color=color_jDMD}, Coordinates(num_train, jerr_jDMD)),
-    Legend(["Nominal MPC", "EDMD", "JDMD"])
-)
-pgfsave(joinpath(BilinearControl.FIGDIR, "airplane_jacobian_error.tikz"), p_err, 
-    include_preamble=false)
+# p_jerr = @pgf Axis(
+#     {
+#         xmajorgrids,
+#         ymajorgrids,
+#         # ymode="log",
+#         xlabel = "Number of training trajectories",
+#         ylabel = "Jacobian Error",
+#         legend_style="{at={(0.97,0.65)},anchor=east}"
+#     },
+#     PlotInc({lineopts..., color=color_nominal}, Coordinates(num_train, jerr_nom)),
+#     PlotInc({lineopts..., color=color_eDMD}, Coordinates(num_train, jerr_eDMD)),
+#     PlotInc({lineopts..., color=color_jDMD}, Coordinates(num_train, jerr_jDMD)),
+#     Legend(["Nominal MPC", "EDMD", "JDMD"])
+# )
+# pgfsave(joinpath(BilinearControl.FIGDIR, "airplane_jacobian_error.tikz"), p_err, 
+#     include_preamble=false)
 ## TODO: Add waypoints plot
